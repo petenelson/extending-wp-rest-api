cfffae9dd6040840ac899fc8a46a0868f33d7c77
Pete Nelson <pete@petenelson.com>
2015-07-09 15:48:54 -0500

    initial commit

---
+++ includes/class-extending-wp-rest-api-controller.php
@@ -0,0 +1,18 @@
+<?php
+
+if ( ! class_exists( 'Extending_WP_REST_API_Controller' ) ) {
+
+
+	class Extending_WP_REST_API_Controller {
+
+		public function plugins_loaded() {
+
+
+		}
+
+
+
+	}
+
+
+}
