3a80edd9186539a41eed938d36766f3877ffbae8
Pete Nelson <pete@petenelson.com>
2015-07-09 15:34:59 -0500

    implemented admin ajax posts

--- includes/class-extending-wp-rest-api.php
+++ includes/class-extending-wp-rest-api.php
@@ -7,9 +7,27 @@ if ( ! class_exists( 'Extending_WP_REST_API' ) ) {
 
 		public function plugins_loaded() {
 
-			//add_action( 'wp_ajax_api-extend-posts', array( $this, 'ajax_get_posts') );
-			//add_action( 'wp_ajax_api-extend-posts', array( $this, 'ajax_get_posts') );
+			// example of admin-ajax got getting some posts
+			add_action( 'wp_ajax_api-extend-posts', array( $this, 'ajax_get_posts'), 1 );
+			add_action( 'wp_ajax_nopriv_api-extend-posts', array( $this, 'ajax_get_posts'), 1 );
 
+		}
+
+
+		public function ajax_get_posts() {
+			global $post;
+
+			$posts = array();
+			$query = new WP_Query( array( 'post_type' => 'post' ) );
+
+			while ( $query->have_posts() ) {
+				$query->the_post();
+				$posts[] = $post;
+			}
+
+			wp_reset_postdata();
+
+			wp_send_json( $posts );
 
 		}
 
