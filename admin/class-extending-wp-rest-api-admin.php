f2ee40a5a58a1fe39fc92f05364dcc71067b5067
Pete Nelson <pete@petenelson.com>
2015-07-22 19:03:15 -0500

    initial commit of the admin interface

---
+++ admin/class-extending-wp-rest-api-admin.php
@@ -0,0 +1,14 @@
+<?php
+
+if ( ! class_exists( 'Extending_WP_REST_API_Admin' ) ) {
+
+
+	class Extending_WP_REST_API_Admin {
+
+		public function plugins_loaded() {
+
+		}
+
+	}
+
+}
--- plugin.php
+++ plugin.php
@@ -7,6 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) die( 'restricted access' );
 
 require_once plugin_dir_path( __FILE__ ) .'includes/class-extending-wp-rest-api-admin-ajax.php';
 require_once plugin_dir_path( __FILE__ ) .'includes/class-extending-wp-rest-api-controller.php';
+require_once plugin_dir_path( __FILE__ ) .'admin/class-extending-wp-rest-api-admin.php';
 
 
 $admin_ajax = new Extending_WP_REST_API_Admin_Ajax();
@@ -16,3 +17,6 @@ add_action( 'plugins_loaded', array( $admin_ajax, 'plugins_loaded') );
 $api_controller = new Extending_WP_REST_API_Controller();
 add_action( 'rest_api_init', array( $api_controller, 'rest_api_init') );
 add_action( 'plugins_loaded', array( $api_controller, 'plugins_loaded') );
+
+$admin = new Extending_WP_REST_API_Admin();
+add_action( 'plugins_loaded', array( $admin, 'plugins_loaded') );
